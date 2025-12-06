08076a8b0225ef1d610b7328adfebeeb72f43eb6
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2025-12-06 06:21:26 +0100

    Turbo

--- cupyturbo/dns_main.py
+++ cupyturbo/dns_main.py
@@ -209,8 +209,7 @@ COLOR_MAPS = {
     "Doom": DOOM_FIRE_LUT,
 }
 
-DEFAULT_CMAP_NAME = "Doom Fire"
-
+DEFAULT_CMAP_NAME = "Turbo"
 
 class MainWindow(QMainWindow):
     def __init__(self, sim: NumPyDnsSimulator) -> None:
@@ -304,7 +303,7 @@ class MainWindow(QMainWindow):
         # CFL selector
         self.cfl_combo = QComboBox()
         self.cfl_combo.setToolTip("L: Controlling Δt (CFL)")
-        self.cfl_combo.addItems(["0.15", "0.25", "0.50", "0.75", "0.95"])
+        self.cfl_combo.addItems(["0.05", "0.15", "0.25", "0.50", "0.75", "0.95"])
         self.cfl_combo.setCurrentText(str(self.sim.cfl))
 
         # Steps selector
@@ -362,7 +361,6 @@ class MainWindow(QMainWindow):
         self._sim_start_iter = self.sim.get_iteration()
 
         # initial draw (omega mode)
-        self.variable_combo.setCurrentIndex(3)
         self.sim.set_variable(self.sim.VAR_OMEGA)
 
         # (optional) a nice colormap index
