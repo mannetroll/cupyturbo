448191138d36bd43207c1165249ce3994253572c
Torbjörn Sjögren <torbjorn.sjogren@gmail.com>
2025-12-06 06:12:29 +0100

    1E15

--- cupyturbo/dns_main.py
+++ cupyturbo/dns_main.py
@@ -284,7 +284,7 @@ class MainWindow(QMainWindow):
         # Reynolds selector (Re)
         self.re_combo = QComboBox()
         self.re_combo.setToolTip("R: Reynolds Number (Re)")
-        self.re_combo.addItems(["1", "1000", "10000", "100000", "1E6", "1E9", "1E12"])
+        self.re_combo.addItems(["1", "1000", "10000", "100000", "1E6", "1E9", "1E12", "1E15"])
         self.re_combo.setCurrentText(str(int(self.sim.re)))
 
         # K0 selector
